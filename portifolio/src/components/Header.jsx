a0559fada2f5f2b8e9498eb2bbb8d5a0b23822b8
leocorreamello <leo.correamello@gmail.com>
2024-09-21 20:04:59 -0300

    nav finalizado

--- portifolio/src/components/Header.jsx
+++ portifolio/src/components/Header.jsx
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router-dom";
+import logo from "../../public/logo.png";
 
 export default function Header() {
     return (
         <>
-            <header className="bg-purple-800 flex text-white justify-around h-14 items-center">
+            <header className="bg-black flex text-white justify-around h-20 items-center text-2xl">
                 <div>
-                    <h1 className="font-bold">Leonardo Mello</h1>
+                    <img className="w-1/4" src={logo} alt="logo" />
                 </div>
                 <nav>
-                    <ul className="flex gap-4">
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/sobre">Sobre</NavLink></li>
+                    <ul className="flex gap-6">
+                        <li><NavLink to="/">Principal</NavLink></li>
+                        <li><NavLink to="/sobre">Sobre mim</NavLink></li>
                         <li><NavLink to="/projetos">Projetos</NavLink></li>
                     </ul>
                 </nav>
